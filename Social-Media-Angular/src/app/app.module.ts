f9abcb8f0b3a61e36d1973b82c00f247c2717bd7
SBK-DS <syedbasitkhurshid@gmail.com>
2023-10-20 01:45:56 +0500

    configurations setup

---
+++ Social-Media-Angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp'
+import { environment } from 'src/environments/environments';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(){
+    FirebaseTSApp.init(environment.firebaseConfig)
+  }
+}
---
+++ Social-Media-Angular/src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    
+    production: true,
+
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    
+        apiKey: "AIzaSyBatXLbMEl8j5fkutAkdBNVcwDJf-lR6I0",
+        authDomain: "social-media-site-angular.firebaseapp.com",
+        projectId: "social-media-site-angular",
+        storageBucket: "social-media-site-angular.appspot.com",
+        messagingSenderId: "1087376755956",
+        appId: "1:1087376755956:web:b3312d2ce27427bed162a1",
+        measurementId: "G-C16TSXK8M2"
+
+  }
+}
