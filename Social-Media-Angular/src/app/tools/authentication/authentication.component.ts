3d693e913b8dee18ad637e6793e5473141e1eaa9
SBK-DS <syedbasitkhurshid@gmail.com>
2023-10-20 19:35:15 +0500

    bottom form completed

--- Social-Media-Angular/src/app/tools/authentication/authentication.component.ts
+++ Social-Media-Angular/src/app/tools/authentication/authentication.component.ts
@@ -5,6 +5,50 @@ import { Component } from '@angular/core';
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
-export class AuthenticationComponent {
 
+export class AuthenticationComponent {
+  
+  state = AuthenticatorCompState.LOGIN;
+
+  constructor() {}
+
+  onForgotPasswordClick() {
+    this.state = AuthenticatorCompState.FORGOT_PASSWORD
+  }
+  onCreateAccountClick() {
+    this.state = AuthenticatorCompState.REGISTER
+  }
+  onLoginClick(){
+    this.state = AuthenticatorCompState.LOGIN
+  }
+
+  isLoginState() {
+    return this.state == AuthenticatorCompState.LOGIN
+  }
+
+  isRegisterState() {
+    return this.state == AuthenticatorCompState.REGISTER
+  }
+
+  isForgotPasswordState() {
+    return this.state == AuthenticatorCompState.FORGOT_PASSWORD
+  }
+
+  getStateText(){
+    switch(this.state)
+    {
+      case AuthenticatorCompState.LOGIN:
+        return 'Login'
+      case AuthenticatorCompState.FORGOT_PASSWORD:
+        return 'Forgot Password'
+      case AuthenticatorCompState.REGISTER:
+        return 'Register'
+    }
+  }
+}
+
+export enum AuthenticatorCompState {
+  LOGIN,
+  REGISTER,
+  FORGOT_PASSWORD
 }
